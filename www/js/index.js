da58cfc0849a10408dbc84dd3c7e113f2f4c69e4
Rizwan Abbas <rizwanaabbas@gmail.com>
2016-02-13 20:24:52 +0400

    css fix and image location changed

--- www/js/index.js
+++ www/js/index.js
@@ -64,7 +64,7 @@ function fixWrapHeight () {
     //document.getElementById('deviceready').style.width = (w) + "px";
     //document.getElementById('app').style.height = (h) + "px";
     //alert(h);
-    //jQuery('#debug').html(jQuery('.app').css('background-image'));
+    jQuery('#debug').html(jQuery('.app').css('background-image')+ ' Height: '+h );
     //document.getElementById('app').style.width = (w) + "px";
 }
 function fixHeight() {
